e0a5508c340988283ded8a201afba67cb2288180
Khayo <Khayo7mhmd@gmail.com>
2020-03-07 14:47:19 -0300

    custom user profile registered

--- likee/settings.py
+++ likee/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'profiles.apps.ProfilesConfig',
     'subscribe.apps.SubscribeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
