1451a9ebc62b09cb1917817439bd7efe86f7220f
Khayo <Khayo7mhmd@gmail.com>
2020-03-08 14:19:56 -0300

    before merging version-1 with master

---
+++ datasabes/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+class subscribe(models.Model):   
+    title = models.CharField(max_length=255)
+    owner = models.CharField(max_length = 60)
+    status = models.CharField(max_length =  60)
+    body = models.TextField()
+    created_at = models.DateTimeField('created_at')
+
+
+class vehicle(models.Model):
+    plate_no = models.CharField('plate Number', max_length=20)
+    vehicle_code = models.CharField('vehicle code', max_length=20)
+    vehicle_model = models.CharField(max_length=30)
+    owner = models.CharField(max_length = 60)
+    status = models.CharField(max_length = 60)
+    created_at = models.DateTimeField('created_at')
--- profiles/urls.py
+++ profiles/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    # url(r'^mob_api/$', views.mobile_api.as_view()),
-    url(r'^mob_api/$', views.abc),
+    url(r'^abc/$', views.abc),
+    url(r'^mob_api/$', views.mobile_api.as_view()),
 ]
